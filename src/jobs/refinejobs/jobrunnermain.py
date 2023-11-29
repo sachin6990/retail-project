0ca897bdb72259799245f68f6aecd20f79458401
sachin6990 <sachinrathod6990@gmail.com>
2023-11-29 18:13:11 +0530

    modified jobrunnermain

--- src/jobs/refinejobs/jobrunnermain.py
+++ src/jobs/refinejobs/jobrunnermain.py
@@ -1,11 +1,8 @@
-import boto3
 import sys
-from utils.vaultutil import VaultClient
-from utils.awsutil import AWSConnector
-sys.path.append('/D/snowflake_project/retail-project/src/jobs/utils')
-#from utils import VaultClient
-#from utils.awsUtil import AWSConnector
+sys.path.append(r'D:\snowflake_project\retail-project\src\jobs\utils')
 
+from vaultutil import VaultClient
+from awsutil import AWSConnector
 
 VAULT_URL = "http://127.0.0.1:8200"
 ROLE_ID = "c306d891-f5e2-a3e7-24f6-e97bd019e48d"
@@ -18,26 +15,21 @@ token = vault_client.authenticate_with_approle()
 if token:
     secret_data = vault_client.get_secret(token)
     if secret_data:
+        aws_access_key = secret_data['data']['bw-aws-accesskey']
+        aws_secret_key = secret_data['data']['bw-aws-secretkey']
         print("Secret data:", secret_data)
     else:
         print("Failed to retrieve secret.")
 else:
     print("Failed to authenticate with AppRole.")
 
-
-aws_access_key = secret_data['data']['bw-aws-accesskey']
-aws_secret_key = secret_data['data']['bw-aws-secretkey']
-
 region = 'us-east-1'  # Replace with your preferred AWS region
+aws_connector = AWSConnector(aws_access_key, aws_secret_key, 'iam', region)
 
-
-client='iam'
-aws_connector = AWSConnector(aws_access_key, aws_secret_key, client, region)
-
-# Access the S3 client through the instance
+# Access the IAM client through the instance
 iam_client = aws_connector.aws_client_conn
 
-# Now you can use s3_client to perform S3 operations
+# Now you can use iam_client to perform IAM operations
 response = iam_client.list_groups()
 
 print("IAM groups:", response)
